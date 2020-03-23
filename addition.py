4bea3ea58db98daa46d43793260871c59cf10056
maxmarzolf <max@treesfromspace.com>
2020-03-23 10:15:41 -0400

    return average time it takes to complete each level

--- addition.py
+++ addition.py
@@ -4,24 +4,29 @@ import datetime
 count = 0
 level = 0
 game_range = 100
+lap_time = []
 
 
 def random_addition():
     global count
     global level
     global game_range
+    global lap_time
     number1 = rand.randrange(game_range)
     number2 = rand.randrange(game_range)
     print(number1, '+', number2, '=')
     start_timer = get_time()
     answer = input()
-    print(get_time() - start_timer)
+    lap = get_time() - start_timer
+    lap_time.append(lap.seconds)
     correct_answer = (number1 + number2)
     if int(answer) == correct_answer:
         is_true = True
         count += 1
         if count % 10 == 0:
             game_range = game_range * 10
+            average_time = sum(lap_time)/len(lap_time)
+            print('Your average time was ', average_time, ' seconds')
         call_function(is_true)
     else:
         is_true = False
