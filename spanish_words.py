87bbe2b6d73ce19cbbf74c26439c4d44ec3efa08
maxmarzolf <max@treesfromspace.com>
2020-06-11 19:39:22 -0400

    first 50 in dictionary of 100

--- spanish_words.py
+++ spanish_words.py
@@ -2,5 +2,10 @@
     'de': 'of; from', 'la': 'the', 'que': 'that, which', 'el': 'the', 'en': 'in, on', 'y': 'and', 'a': 'to, at', 'los': 'the',
     'se': '-self, oneself', 'del': 'from the', 'las': 'the', 'un': 'a, an', 'por': 'by, for, through', 'con': 'with', 'no': 'no',
     'una': 'a, an, one', 'su': 'his, her, its, your', 'para': 'for, to, in order to', 'es': 'is', 'al': 'to the', 'lo': 'the',
-    'como': 'like, as', 'más': 'more', 'o': 'or', 'pero': 'but'
+    'como': 'like, as', 'más': 'more', 'o': 'or', 'pero': 'but', 'sus': 'your', 'le': 'third person indirect object', 'ha': 'he, she, it, has done something',
+    'me': 'me', 'si': 'if, whether', 'sin': 'without', 'sobre': 'on top of, over, about', 'este': 'this', 'ya': 'already, still',
+    'entre': 'between', 'cuando': 'when', 'todo': 'all, every', 'esta': 'this', 'ser': 'to be', 'son': 'they are', 'dos': 'two',
+    'también': 'too, also, as well', 'fue': 'was', 'había': 'used to be', 'era': 'was', 'muy': 'very', 'años': 'years',
+    'hasta': 'until', 'desde': 'from, since', 'está': 'is'
+
 }
