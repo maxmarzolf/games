f006cfb08f8ab83ac68f68ed022004f108d505c6
maxmarzolf <max@treesfromspace.com>
2020-06-11 16:07:43 -0400

    first 25 in dictionary of 100

---
+++ spanish_words.py
@@ -0,0 +1,6 @@
+100_most_common_spanish_words = {
+    'de': 'of; from', 'la': 'the', 'que': 'that, which', 'el': 'the', 'en': 'in, on', 'y': 'and', 'a': 'to, at', 'los': 'the',
+    'se': '-self, oneself', 'del': 'from the', 'las': 'the', 'un': 'a, an', 'por': 'by, for, through', 'con': 'with', 'no': 'no',
+    'una': 'a, an, one', 'su': 'his, her, its, your', 'para': 'for, to, in order to', 'es': 'is', 'al': 'to the', 'lo': 'the',
+    'como': 'like, as', 'más': 'more', 'o': 'or', 'pero': 'but'
+}
